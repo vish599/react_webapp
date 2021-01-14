a234642e8978167ef6a7f44cf674018f3f785073
vish599 <vishalpal599@gmail.com>
2021-01-14 14:10:15 +0530

    my_commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Container/Home';
+import Header from './Components/Header';
+import Hero from './Components/Hero';
+
+import Post from './Container/Post/post';
+import Layout from './Components/Layout';
+import Recant from './Components/Recant';
+import Homo from './Components/Homo/homo';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header />
+    <Hero />
+      <Home />
+     <Post />
+     <Layout />
+     <Layout />
+     <Layout />
+     <Layout />
+     <Recant />
+     <Homo />
+     <Home />
+     
     </div>
   );
 }
